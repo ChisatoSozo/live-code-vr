1ef4f64ef34a537333b81b145e88c09f4c15cec8
ChisatoSozo <sozochisato@gmail.com>
2021-05-27 02:30:00 -0400

    demo

--- src/scene/Scene.tsx
+++ src/scene/Scene.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import { PhysicsImpostor, Vector3 } from '@babylonjs/core'
+import React, { Suspense } from 'react'
+import { Model } from 'react-babylonjs'
+import { Grabbable } from '../engine/interactions/Grabbable'
 import { LiveCodeVR } from '../engine/LiveCodeVR'
 
 export const Scene = () => {
     return (
         <LiveCodeVR>
-            {/* <Grabbable>
-                <sphere name="sphere" diameter={0.4} segments={16} position={new Vector3(0, 2.7, 0)}>
+            <Grabbable grabCallback={() => { }}>
+                <sphere name="sphere" diameter={0.4} segments={16} position={new Vector3(0, 2.8, 0)}>
                     <physicsImpostor type={PhysicsImpostor.SphereImpostor} _options={{
                         mass: 1,
-                        restitution: 0.9
+                        restitution: 1.5
                     }} />
                 </sphere>
             </Grabbable>
             <Suspense fallback={<box name='fallback' position={new Vector3(0, 1.1, 0)} />}>
                 <Model name="wriggle" rootUrl={`/wriggle/`} sceneFilename='wriggle.glb' position={new Vector3(0, 1.1, 0)} />
-            </Suspense> */}
+            </Suspense>
         </LiveCodeVR>
     )
 }
